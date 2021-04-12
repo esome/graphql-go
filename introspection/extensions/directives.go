d0ced4acc9d7c2a1ac68e099dabfc05969ae9047
Emilija Ivanovic <emilija.ivanovic@esome.com>
2021-04-12 16:24:23 +0200

    wip: making directive args accessible

--- introspection/extensions/directives.go
+++ introspection/extensions/directives.go
@@ -3,7 +3,10 @@
 
 package extensions
 
-import "github.com/graph-gophers/graphql-go/internal/common"
+import (
+	"encoding/json"
+	"github.com/graph-gophers/graphql-go/internal/common"
+)
 
 // NewDirectiveList creates a new *DirectiveList out of the given internal one
 func NewDirectiveList(list common.DirectiveList) DirectiveList {
@@ -21,16 +24,16 @@ func NewDirectiveList(list common.DirectiveList) DirectiveList {
 			continue
 		}
 
-		args := make([]DirectiveArg, len(d.Args))
-		for j, a := range d.Args {
-			args[j] = DirectiveArg{
-				Name:  a.Name.Name,
-				Value: a.Value.String(),
+		args := make(map[string]DirectiveArg, len(d.Args))
+		for _, arg := range d.Args {
+			args[arg.Name.Name] = DirectiveArg{
+				Name:  arg.Name.Name,
+				Value: arg.Value.Value(nil),
 			}
 		}
 		directives[d.Name.Name] = DirectiveItem{
 			Name: d.Name.Name,
-			Args: &args,
+			Args: DirectiveArgList{args},
 		}
 
 	}
@@ -65,11 +68,35 @@ func (l DirectiveList) List() []DirectiveItem {
 // DirectiveItem represents a directive attached to an object, field, etc. directly in the schema
 type DirectiveItem struct {
 	Name string
-	Args *[]DirectiveArg
+	Args DirectiveArgList
+}
+
+// Get returns a mapped directive argument by its given name.
+// If the directive argument cannot be found (e.g. invalid or not attached) the method returns nil
+func (l DirectiveArgList) Get(name string, dest interface{}) (bool, error) {
+	if arg, ok := l.args[name]; ok {
+		b, err := json.Marshal(arg.Value)
+		if err != nil {
+			return false, err
+		}
+
+		err = json.Unmarshal(b, dest)
+		if err != nil {
+			return false, err
+		}
+
+		return true, nil
+	}
+
+	return false, nil
+}
+
+type DirectiveArgList struct {
+	args map[string]DirectiveArg
 }
 
 // DirectiveArg sdf
 type DirectiveArg struct {
 	Name  string
-	Value string
+	Value interface{}
 }
